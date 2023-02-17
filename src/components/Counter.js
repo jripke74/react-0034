202450c437e32651cd892a95592c85696774e75c
Jeff Ripke <jripke74@gmail.com>
2023-02-16 17:16:37 -0800

    add toggle logic

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,25 +5,28 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 })
-  }
+    dispatch({ type: 'increase', amount: 5 });
+  };
 
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Icrease by 5</button>
