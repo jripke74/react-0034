2678182e99436dfcf90f8d61b5cdc15685e4a591
Jeff Ripke <jripke74@gmail.com>
2023-02-20 16:11:15 -0800

    add export for counterActions

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ const counterSlice = createSlice({
       state.counter++;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
     decrement(state) {
       state.counter--;
@@ -24,5 +24,7 @@ const counterSlice = createSlice({
 const store = configureStore({
   reducer: counterSlice.reducer,
 });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
